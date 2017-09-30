ea4a758a86aba5c819691ae5906dd6b2f9615515
Jerelyn Acosta <jacostaa8@gmail.com>
2017-09-30 17:23:07 +0000

    This is my todo list

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,5 +1,10 @@
 class TodoController<ApplicationController
     def index 
     end
+    def show 
+        @todo_description = "finish u.s. history research paper"
+        @todo_pomodoro_estimate=4
+        
+    end
     
 end
