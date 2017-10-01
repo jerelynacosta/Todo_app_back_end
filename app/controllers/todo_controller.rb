edd66fdd751d62aa4c2ec0d1264dad065ecb32f3
Jerelyn Acosta <jacostaa8@gmail.com>
2017-10-01 19:16:25 +0000

    these are my changes

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -36,4 +36,16 @@ class TodoController<ApplicationController
         #     @todo_pomodoro_estimate= 4
         # end
     end
+    def new 
+    end
+   
+    def create
+        t= Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate= params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+        
+    end
+        
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'todo/new' , to: 'todo#new'
+  get 'todo/create', to: 'todo#create'
   get 'todo/index' , to: 'todo#index'
   get 'todo/show/:id' , to: 'todo#show'
-  
+
   
   
   
